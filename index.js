937b140c756bd4425a2de42133d8da2757ab1cab
+ <+>
2022-11-27 13:12:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    let score = record.find((score)=>{
+        if (score.result==='W'){
+            return true;
+        }
+    })
+        if (score){
+            return score.year
+        }
+}
